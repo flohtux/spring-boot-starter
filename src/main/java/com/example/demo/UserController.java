4fe7ac523c1324518314ffd1dda7c9f5595e905b
Florian Selent <fselent@googlemail.com>
2024-10-24 22:21:12 +0200

    Initialize.

--- src/main/java/com/example/demo/UserController.java
+++ src/main/java/com/example/demo/UserController.java
@@ -3,8 +3,6 @@ package com.example.demo;
 import java.util.Collections;
 import java.util.Map;
 
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,8 +10,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
     	
     @GetMapping("/user")
-    public Map<String, Object> user(@AuthenticationPrincipal OAuth2User principal) {
-        return Collections.singletonMap("name", principal.getAttribute("name"));
+    public Map<String, Object> user() {
+        return Collections.singletonMap("name", "Florian");
     }
-    
+
 }
